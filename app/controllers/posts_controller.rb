7206819e2611bc46bbce6e6f6133d444bb659279
Mai Fujiwara <mybow008@piano.ocn.ne.jp>
2020-04-11 03:05:32 +0900

    eighth commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,19 @@
 class PostsController < ApplicationController
   def top
+    @posts = @posts.order("id desc")
+    @user = User.find(current_user.id)
   end
   
   def upload
+    @post = Post.new
   end
   
   def create
-  end
-  
-  def profiles
-  end
-  
-  def manage
+    @post = Post.new(post_params)
   end
   
   def artworks
+    @post =Post.find(params[:id])
   end
   
 end
